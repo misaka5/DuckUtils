a737a0b6aeb85d3334555e3575318ed5ddd12fec
Quant1um <spitfirexv22@gmail.com>
2019-12-05 18:41:08 +0300

    version change

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -3,4 +3,4 @@ using System.Reflection;
 [assembly: AssemblyTitle("DuckUtils")]
 [assembly: AssemblyCompany("misaka5")]
 [assembly: AssemblyDescription("Mod that adds random stuff to the game")]
-[assembly: AssemblyVersion("1.1")]
+[assembly: AssemblyVersion("1.1.0.1")]
